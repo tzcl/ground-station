4aa0444f2925bd11cefff78a05766ea5b3922bf3
Toby Law <toby@tzcl.me>
2021-09-10 19:10:38 +1000

    Set up testing for pi -> arduino comm

--- ground-station.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import serial
-
-if __name__ == "__main__":
-    print("Hello world!")
-    name = input("Please enter your name\n")
-    print("Hello, " + name)
---
+++ station.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import serial
+import time
+import csv
+
+conn = serial.Serial('COM5', 9600)
+time.sleep(2)  # wait for the serial connection to initialise
+
+# passes are stored in rows of timestamp,altitude,azimuth
+with open('test.csv') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        conn.write(row['altitude'], row['azimuth'])
+        time.sleep(1)
+
+conn.close()
